8db89f887e008d601be44623d0897a9abd8e719e
grijeshkumar <patwagrijesh@gmail.com>
2025-09-13 17:10:47 +0530

    .

--- ToDo/todoapp/models.py
+++ ToDo/todoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Transaction(models.Model):
+   title = models.CharField(max_length=250)
+   description = models.CharField(max_length=100)
+   created_at = models.DateTimeField(auto_now=True)
+   update_at = models.DateTimeField(auto_now_add=True)
