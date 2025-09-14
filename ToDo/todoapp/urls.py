7fe093e09506fcf4b76f5ae9ce916dfffa4afd2d
grijeshkumar <patwagrijesh@gmail.com>
2025-09-14 12:07:38 +0530

    added view set routes

--- ToDo/todoapp/urls.py
+++ ToDo/todoapp/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
+from .views import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'todo',TodoViewSet)
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('',include(router.urls))
 ]
--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Transaction
+from .serializers import TodoSerializer
 
 # Create your views here.
 
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TodoSerializer
+
 def home(request):
     return HttpResponse("Hello from todoapp")
+
+
 
